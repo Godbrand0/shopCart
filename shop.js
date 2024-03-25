56c896024781942fdfa93b0cbb1809eebf4df589
eregha thompson <thompsoneregha005@gmail.com>
2024-03-25 04:40:01 -0700

    first commit

--- shop.js
+++ shop.js
@@ -12,3 +12,56 @@ openShopping.addEventListener('click', () =>{
 closeShopping.addEventListener('click', ()=> {
     body.classList.remove('active');
 })
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+        image: '1.PNG',
+        price: 120000
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: '2.PNG',
+        price: 126000
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: '3.PNG',
+        price: 174000
+    },
+    {
+        id: 4,
+        name: 'PRODUCT NAME 4',
+        image: '4.PNG',
+        price: 300000
+    },
+    {
+        id: 5,
+        name: 'PRODUCT NAME 5',
+        image: '2.PNG',
+        price: 1000000
+    },
+    {
+        id: 6,
+        name: 'PRODUCT NAME 6',
+        image: '6.PNG',
+        price: 1976000
+    }
+];
+let listCards = [];
+function initApp() {
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.innerHTML = `
+            <img src="image/${value.image}"/>
+            <div class="title">${value.name}</div>
+            <div class="price">${value.price.toLocaleString()}</div>
+            <button onclick="addtocart(${key})">Add To Cart</button>
+        `;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
